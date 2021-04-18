066587797cf4a757b720efe8e6e1b3af48459fa3
kavfox <kavcoffeefox>
2021-04-18 13:52:51 +0300

    Добавлен SQL скрипт для создания таблиц

---
+++ create_table.sql
@@ -0,0 +1,27 @@
+create table people(
+                       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                       firstName TEXT,
+                       lastName TEXT,
+                       patronymic TEXT,
+                       birthDay TEXT,
+                       department TEXT,
+                       position TEXT,
+                       rank TEXT
+);
+
+create table task(
+                     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                     name,
+                     period,
+                     deadline,
+                     description,
+                     type,
+                     complete
+);
+
+CREATE TABLE task_executors(
+                               task_id INTEGER,
+                               people_id INTEGER,
+                               FOREIGN KEY(task_id) REFERENCES task(id) ON DELETE CASCADE,
+                               FOREIGN KEY(people_id) REFERENCES people(id) ON DELETE CASCADE
+);
