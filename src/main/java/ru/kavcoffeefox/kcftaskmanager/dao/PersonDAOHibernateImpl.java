ff9f5107595fd80c1016065aa21de5ce913499b7
kavfox <kavcoffeefox>
2021-04-18 20:16:44 +0300

    Реализованы CRUD операции в DAO классах

--- src/main/java/ru/kavcoffeefox/kcftaskmanager/dao/PersonDAOHibernateImpl.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/dao/PersonDAOHibernateImpl.java
@@ -1,32 +1,88 @@
 package ru.kavcoffeefox.kcftaskmanager.dao;
 
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import ru.kavcoffeefox.kcftaskmanager.database.HibernateConnection;
 import ru.kavcoffeefox.kcftaskmanager.entity.Person;
 
 import java.util.List;
 
+@Slf4j
 public class PersonDAOHibernateImpl implements PersonDAO{
     @Override
-    public boolean addPerson() {
-        return false;
+    public boolean addPerson(Person person) {
+        try(Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()){
+            session.beginTransaction();
+            session.persist(person);
+            session.getTransaction().commit();
+            return true;
+        }
+        catch (Exception e){
+            log.error(e.getMessage());
+            return false;
+        }
     }
 
     @Override
     public Person getPerson(Integer id) {
-        return null;
+        try(Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()){
+            session.beginTransaction();
+            Person person = session.get(Person.class, id);
+            session.getTransaction().commit();
+            return person;
+        }
+        catch (Exception e){
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     @Override
     public boolean deletePerson(Integer id) {
-        return false;
+        try(Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()) {
+            session.beginTransaction();
+            Person person = session.get(Person.class, id);
+            session.delete(person);
+            session.getTransaction().commit();
+            return true;
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return false;
+        }
     }
 
     @Override
     public boolean updatePerson(Integer id, Person person) {
-        return false;
+        try (Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()) {
+            session.beginTransaction();
+            Person localPerson = session.get(Person.class, id);
+            localPerson.setFirstName(person.getFirstName());
+            localPerson.setLastName(person.getLastName());
+            localPerson.setPatronymic(person.getPatronymic());
+            localPerson.setBirthDay(person.getBirthDay());
+            localPerson.setDepartment(person.getDepartment());
+            localPerson.setRank(person.getRank());
+            localPerson.setPosition(person.getPosition());
+            localPerson.setTasks(person.getTasks());
+            session.saveOrUpdate(localPerson);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+        return true;
     }
 
     @Override
     public List<Person> getAllPerson() {
-        return null;
+        try (Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()) {
+            session.beginTransaction();
+            List<Person> persons = session.createQuery("from Person").getResultList();
+            session.getTransaction().commit();
+            return persons;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
     }
 }
--- src/main/java/ru/kavcoffeefox/kcftaskmanager/dao/TaskDAOHibernateImpl.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/dao/TaskDAOHibernateImpl.java
@@ -1,34 +1,89 @@
 package ru.kavcoffeefox.kcftaskmanager.dao;
 
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
+import ru.kavcoffeefox.kcftaskmanager.database.HibernateConnection;
 import ru.kavcoffeefox.kcftaskmanager.entity.Task;
 
 import java.time.LocalDate;
 import java.util.List;
 
-public class TaskDAOHibernateImpl implements TaskDAO{
+@Slf4j
+public class TaskDAOHibernateImpl implements TaskDAO {
+
     @Override
     public boolean addTask(Task task) {
-        return false;
+        try (Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()) {
+            session.beginTransaction();
+            session.persist(task);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+        return true;
     }
 
     @Override
     public Task getTask(Integer id) {
-        return null;
+        Task task;
+        try (Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()) {
+            session.beginTransaction();
+            task = session.get(Task.class, id);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
+        return task;
     }
 
     @Override
     public boolean deleteTask(Integer id) {
-        return false;
+        try (Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()) {
+            session.beginTransaction();
+            Task task = session.get(Task.class, id);
+            session.delete(task);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean updateTask(Integer id, Task task) {
-        return false;
+        try (Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()) {
+            session.beginTransaction();
+            Task localTask = session.get(Task.class, id);
+            localTask.setComplete(task.isComplete());
+            localTask.setDeadline(task.getDeadline());
+            localTask.setDescription(task.getDescription());
+            localTask.setExecutors(task.getExecutors());
+            localTask.setName(task.getName());
+            localTask.setPeriod(task.getPeriod());
+            localTask.setType(task.getType());
+            session.saveOrUpdate(localTask);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return false;
+        }
+        return true;
     }
 
     @Override
     public List<Task> getAllTask() {
-        return null;
+        try (Session session = HibernateConnection.getInstance().getFactory().getCurrentSession()) {
+            session.beginTransaction();
+            List<Task> tasks = session.createQuery("from Task").getResultList();
+            session.getTransaction().commit();
+            return tasks;
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return null;
+        }
     }
 
     @Override
