02f6e23376999f669d6b99cc344bfcd8cdd6eda7
kavfox <kavcoffeefox>
2021-04-18 15:06:45 +0300

    Добавлен класс HibernateConnection который будет предоставлять доступ к сессии hibernate.

---
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/database/HibernateConnection.java
@@ -0,0 +1,33 @@
+package ru.kavcoffeefox.kcftaskmanager.database;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import ru.kavcoffeefox.kcftaskmanager.entity.Person;
+import ru.kavcoffeefox.kcftaskmanager.entity.Task;
+
+public class HibernateConnection {
+    private volatile static HibernateConnection INSTANCE;
+    private volatile static SessionFactory factory;
+
+    private HibernateConnection(){
+        factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Task.class)
+                .addAnnotatedClass(Person.class)
+                .buildSessionFactory();
+    }
+
+    public static HibernateConnection getInstance(){
+        if (INSTANCE == null)
+            synchronized (HibernateConnection.class) {
+                if (INSTANCE == null) {
+                    INSTANCE = new HibernateConnection();
+                }
+            }
+        return INSTANCE;
+    }
+
+    public static SessionFactory getFactory() {
+        return factory;
+    }
+}
