f007d89af14e67b595b64b72181d2da2bbd03ecb
kavfox <kavcoffeefox>
2021-04-18 14:16:06 +0300

    Классы People и Task переделаны для отношения Many-to-many

--- src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/People.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/People.java
@@ -4,39 +4,53 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity @Table(name = "people")
-@Getter @Setter
+@Entity
+@Table(name = "people")
+@Getter
+@Setter
 @NoArgsConstructor
 @ToString
+@Builder
 public class People {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    @ToString.Exclude private int id;
+    @ToString.Exclude
+    private int id;
     @Column(name = "firstName")
-    private  String firstName;
+    private String firstName;
     @Column(name = "lastName")
-    private  String lastName;
+    private String lastName;
     @Column(name = "patronymic")
-    private  String patronymic;
+    private String patronymic;
     @Column(name = "birthDay")
     private LocalDate birthDay;
     @Column(name = "department")
-    private  String department;
+    private String department;
     @Column(name = "position")
-    private  String position;
+    private String position;
     @Column(name = "rank")
-    private  String rank;
+    private String rank;
 
-    public People(String firstName, String lastName, String patronymic, LocalDate birthDay, String department, String position, String rank) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.patronymic = patronymic;
-        this.birthDay = birthDay;
-        this.department = department;
-        this.position = position;
-        this.rank = rank;
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name = "task_executor",
+            joinColumns = @JoinColumn(name = "task_id"),
+            inverseJoinColumns = @JoinColumn(name = "people_id")
+    )
+    @ToString.Exclude
+    private List<Task> tasks;
+
+    public void addTask(Task task) {
+        if (tasks == null) {
+            tasks = new ArrayList<>();
+        }
+        tasks.add(task);
     }
+
 }
--- src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/Task.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/Task.java
@@ -5,17 +5,22 @@ import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-@Entity @Table(name = "task")
-@Getter @Setter @NoArgsConstructor
+@Entity
+@Table(name = "task")
+@Getter
+@Setter
+@NoArgsConstructor
 @ToString
 @Builder
 public class Task {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    @ToString.Exclude private int id;
+    @ToString.Exclude
+    private int id;
     @Column(name = "name")
     private String name;
     @Column(name = "period")
@@ -24,10 +29,26 @@ public class Task {
     private LocalDate deadline;
     @Column(name = "description")
     private String description;
-    @ToString.Exclude private List<People> executor;
     @Column(name = "type")
     private String type;
     @Column(name = "complete")
     private boolean complete;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE,
+    CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(
+            name = "task_executor",
+            joinColumns = @JoinColumn(name = "people_id "),
+            inverseJoinColumns = @JoinColumn(name = "task_id")
+    )
+    @ToString.Exclude
+    private List<People> executors;
+
+    public void addExecutor(People executor) {
+        if (executors == null) {
+            executors = new ArrayList<>();
+        }
+        executors.add(executor);
+    }
 
 }
