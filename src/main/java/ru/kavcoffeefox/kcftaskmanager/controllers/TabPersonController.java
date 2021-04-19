5fe73265189c6370bb48de4911ec4b91981611df
kavfox <kavcoffeefox>
2021-04-19 21:06:42 +0300

    В TabPersonController добавлено отображение модального окна для добавления Person

--- src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/TabPersonController.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/TabPersonController.java
@@ -1,7 +1,13 @@
 package ru.kavcoffeefox.kcftaskmanager.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -20,4 +26,28 @@ public class TabPersonController extends AbstractController{
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
     }
+
+    @FXML
+    public void addPerson(){
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(RootViewController.class.getResource("/view/modalwindows/PersonView.fxml"));
+            AnchorPane page = loader.load();
+
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Редактирование");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(this.getMainStage());
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            PersonViewController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+
+            dialogStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
