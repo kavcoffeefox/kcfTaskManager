1de8a25e1872023879639f982dd4ecf1c7113229
kavfox <kavcoffeefox>
2021-04-19 20:30:34 +0300

    Добавлено представление для отображения списка Person и контроллер для него без реализации. Также добавлен таб для отображения вышеуказанного представления в MainTabsView.

---
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/TabPersonController.java
@@ -0,0 +1,23 @@
+package ru.kavcoffeefox.kcftaskmanager.controllers;
+
+import javafx.stage.Stage;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TabPersonController extends AbstractController{
+    @Override
+    public void setMainStage(Stage mainStage) {
+        super.setMainStage(mainStage);
+    }
+
+    @Override
+    public Stage getMainStage() {
+        return super.getMainStage();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
