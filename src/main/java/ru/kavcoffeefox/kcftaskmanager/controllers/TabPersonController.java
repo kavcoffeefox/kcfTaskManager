bedabc0b94888d1d55547ef4121278db049bd90a
kavfox <kavcoffeefox>
2021-04-22 22:06:26 +0300

    В TabPersonController добавлено контекстное меню для crud Person

--- src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/TabPersonController.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/TabPersonController.java
@@ -5,6 +5,8 @@ import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.AnchorPane;
@@ -18,7 +20,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class TabPersonController extends AbstractController{
+public class TabPersonController extends AbstractController {
     Manager<Person, Integer> personManager;
 
     @FXML
@@ -64,11 +66,37 @@ public class TabPersonController extends AbstractController{
 //        descriptionColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue()));
 
         personTableView.setItems(FXCollections.observableArrayList(personManager.getAll()));
+
+        ContextMenu contextMenu = new ContextMenu();
+
+        MenuItem itemDelete = new MenuItem("Удалить");
+        itemDelete.setOnAction(event -> {
+            if (personTableView.getSelectionModel().getSelectedItem() != null) {
+                personManager.delete(personTableView.getSelectionModel().getSelectedItem().getId());
+                personTableView.getItems().remove(personTableView.getSelectionModel().getSelectedItem());
+            }
+        });
+        MenuItem itemAdd = new MenuItem("Добавить");
+        itemAdd.setOnAction(event -> {
+            this.addPerson();
+        });
+        MenuItem itemUpdate = new MenuItem("Редактировать");
+        itemUpdate.setOnAction(event -> {
+            if (personTableView.getSelectionModel().getSelectedItem() != null) {
+                Person person = personTableView.getSelectionModel().getSelectedItem();
+                System.out.println(person);
+                showPersonView(person);
+                personManager.update(person.getId(), person);
+            }
+        });
+
+        contextMenu.getItems().addAll(itemDelete);
+        contextMenu.getItems().addAll(itemAdd);
+        contextMenu.getItems().addAll(itemUpdate);
+        this.personTableView.setContextMenu(contextMenu);
     }
 
-    @FXML
-    public void addPerson(){
-        Person person = new Person();
+    public void showPersonView(Person person) {
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(TabPersonController.class.getResource("/view/modalwindows/PersonView.fxml"));
@@ -89,6 +117,12 @@ public class TabPersonController extends AbstractController{
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @FXML
+    public void addPerson() {
+        Person person = new Person();
+        showPersonView(person);
         personManager.add(person);
     }
 }
