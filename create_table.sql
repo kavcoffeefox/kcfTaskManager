8fecc674e086588ff421912665e19135a8a9a241
kavfox <kavcoffeefox>
2021-04-22 22:37:38 +0300

    Изменен sql скрипт создания таблиц с учетом новых сущностей Tag, Document и изменениях в Person

--- create_table.sql
+++ create_table.sql
@@ -1,27 +1,80 @@
-create table person(
-                       id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                       firstName,
-                       lastName,
-                       patronymic,
-                       birthDay,
-                       department,
-                       position,
-                       rank
+create table document
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name,
+    path,
+    description,
+    requisite,
+    date
 );
 
-create table task(
-                     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-                     name,
-                     period,
-                     deadline,
-                     description,
-                     type,
-                     complete
+create table tag
+(
+    id,
+    name,
+    description
 );
 
-CREATE TABLE task_executor(
-                       task_id INTEGER,
-                       person_id INTEGER,
-                       FOREIGN KEY(task_id) REFERENCES task(id) ON DELETE CASCADE,
-                       FOREIGN KEY(person_id) REFERENCES person(id) ON DELETE CASCADE
+create table task
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name,
+    period,
+    deadline,
+    description,
+    type,
+    complete
+);
+
+create table person
+(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    firstName,
+    lastName,
+    patronymic,
+    birthDay,
+    department,
+    position,
+    rank,
+    description
+);
+
+CREATE TABLE task_executor
+(
+    task_id   INTEGER,
+    person_id INTEGER,
+    FOREIGN KEY (task_id) REFERENCES task (id) ON DELETE CASCADE,
+    FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
+);
+
+create table task_tag
+(
+    tag_id  INTEGER,
+    task_id INTEGER,
+    FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE,
+    FOREIGN KEY (task_id) REFERENCES task (id) ON DELETE CASCADE
+);
+
+create table document_tag
+(
+    tag_id      INTEGER,
+    document_id INTEGER,
+    FOREIGN KEY (tag_id) REFERENCES tag (id) ON DELETE CASCADE,
+    FOREIGN KEY (document_id) REFERENCES document (id) ON DELETE CASCADE
+);
+
+create table document_person
+(
+    document_id,
+    person_id,
+    FOREIGN KEY (document_id) REFERENCES document (id) ON DELETE CASCADE,
+    FOREIGN KEY (person_id) REFERENCES person (id) ON DELETE CASCADE
+);
+
+create table document_task
+(
+    document_id INTEGER,
+    task_id     INTEGER,
+    FOREIGN KEY (document_id) REFERENCES document (id) ON DELETE CASCADE,
+    FOREIGN KEY (task_id) REFERENCES task (id) ON DELETE CASCADE
 );
