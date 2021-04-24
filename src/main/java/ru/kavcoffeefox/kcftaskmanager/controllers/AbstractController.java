c39faf5ba1a4798a972fb698f6da9cddc4d40884
kavfox <kavcoffeefox>
2021-04-24 22:36:42 +0300

    В AbstractController добавлены методы для модальных окон и закрытия View

--- src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/AbstractController.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/AbstractController.java
@@ -2,20 +2,35 @@ package ru.kavcoffeefox.kcftaskmanager.controllers;
 
 import javafx.fxml.Initializable;
 import javafx.stage.Stage;
+import lombok.extern.slf4j.Slf4j;
+import ru.kavcoffeefox.kcftaskmanager.entity.SimpleItem;
 
 import java.util.ResourceBundle;
 
+@Slf4j
 public abstract class AbstractController implements Initializable {
     private ResourceBundle resourceBundle;
 
     // Ссылка на главное приложение.
     private Stage mainStage;
+    private Stage dialogStage;
 
     public void setMainStage(Stage mainStage) {
         this.mainStage = mainStage;
     }
     public Stage getMainStage() {return this.mainStage;}
 
+    public void setItem(SimpleItem item)
+    {
 
+    }
 
+    public void setDialogStage(Stage dialogStage){
+        this.dialogStage = dialogStage;
+    }
+    public Stage getDialogStage(){ return this.dialogStage;}
+
+    public void closeDialogView(){
+        this.dialogStage.close();
+    }
 }
