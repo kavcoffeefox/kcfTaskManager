2bd7baf1f8bf4e0c27c08686f149eba93bb5c7fe
kavfox <kavcoffeefox>
2021-04-25 21:21:29 +0300

    Переделаны entity Document и Tag без property

--- src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/Document.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/Document.java
@@ -1,140 +1,53 @@
 package ru.kavcoffeefox.kcftaskmanager.entity;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.StringProperty;
-import lombok.NoArgsConstructor;
-import lombok.ToString;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 @Table(name = "document")
-@Access(AccessType.PROPERTY)
-@NoArgsConstructor
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
 @ToString
-public class Document {
+@Builder
+public class Document implements SimpleItem{
 
-    private IntegerProperty id;
-    private StringProperty name;
-    private StringProperty path;
-    private StringProperty description;
-    private StringProperty requisite;
-    private ObjectProperty<LocalDate> date;
-
-//    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
-//    @JoinTable(name = "document_person",
-//    joinColumns = @JoinColumn(name = "document_id"),
-//    inverseJoinColumns = @JoinColumn(name = "person_id"))
-//    private List<Person> persons;
-//    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
-//    private List<Tag> tags;
-//    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
-//    private List<Task> tasks;
-
+    @ToString.Exclude
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    public int getId() {
-        return id.get();
-    }
-
-    public IntegerProperty idProperty() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id.set(id);
-    }
-
+    private int id;
     @Column(name = "name")
-    public String getName() {
-        return name.get();
-    }
-
-    public StringProperty nameProperty() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
+    private String name;
     @Column(name = "path")
-    public String getPath() {
-        return path.get();
-    }
-
-    public StringProperty pathProperty() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path.set(path);
-    }
-
+    private String path;
     @Column(name = "description")
-    public String getDescription() {
-        return description.get();
-    }
-
-    public StringProperty descriptionProperty() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description.set(description);
-    }
-
+    private String description;
     @Column(name = "requisite")
-    public String getRequisite() {
-        return requisite.get();
-    }
-
-    public StringProperty requisiteProperty() {
-        return requisite;
-    }
-
-    public void setRequisite(String requisite) {
-        this.requisite.set(requisite);
-    }
-
+    private String requisite;
     @Column(name = "date")
-    public LocalDate getDate() {
-        return date.get();
-    }
+    private LocalDate date;
 
-    public ObjectProperty<LocalDate> dateProperty() {
-        return date;
-    }
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name = "document_person",
+    joinColumns = @JoinColumn(name = "document_id"),
+    inverseJoinColumns = @JoinColumn(name = "person_id"))
+    private List<Person> persons = new ArrayList<>();
 
-    public void setDate(LocalDate date) {
-        this.date.set(date);
-    }
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name = "document_task",
+            joinColumns = @JoinColumn(name = "document_id"),
+            inverseJoinColumns = @JoinColumn(name = "task_id"))
+    private List<Task> tasks = new ArrayList<>();
 
-//    public List<Person> getPersons() {
-//        return persons;
-//    }
-//
-//    public void setPersons(List<Person> persons) {
-//        this.persons = persons;
-//    }
-//
-//    public List<Tag> getTags() {
-//        return tags;
-//    }
-//
-//    public void setTags(List<Tag> tags) {
-//        this.tags = tags;
-//    }
-//
-//    public List<Task> getTasks() {
-//        return tasks;
-//    }
-//
-//    public void setTasks(List<Task> tasks) {
-//        this.tasks = tasks;
-//    }
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.DETACH, CascadeType.MERGE, CascadeType.REFRESH})
+    @JoinTable(name = "document_tag",
+    joinColumns = @JoinColumn(name = "document_id"),
+    inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private List<Tag> tags = new ArrayList<>();
+
 }
--- src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/Tag.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/Tag.java
@@ -1,8 +1,8 @@
 package ru.kavcoffeefox.kcftaskmanager.entity;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.StringProperty;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import javax.persistence.*;
@@ -11,50 +11,17 @@ import javax.persistence.*;
 @ToString
 @Entity
 @Table(name = "tag")
-@Access(AccessType.PROPERTY)
-public class Tag {
-    private IntegerProperty id;
-    private StringProperty name;
-    private StringProperty description;
-
+@Getter
+@Setter
+public class Tag implements SimpleItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    public int getId() {
-        return id.get();
-    }
-
-    public IntegerProperty idProperty() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id.set(id);
-    }
-
+    private int id;
     @Column(name = "name")
-    public String getName() {
-        return name.get();
-    }
-
-    public StringProperty nameProperty() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name.set(name);
-    }
-
+    private String name;
     @Column(name = "description")
-    public String getDescription() {
-        return description.get();
-    }
+    private String description;
 
-    public StringProperty descriptionProperty() {
-        return description;
-    }
 
-    public void setDescription(String description) {
-        this.description.set(description);
-    }
 }
