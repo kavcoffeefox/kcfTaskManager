8e858dbf812f32590eaa7c2b4536314b7ea00df7
kavfox <kavcoffeefox>
2021-04-25 21:17:35 +0300

    В Person добавлены статический метод для отображения в формате Фамилия-инициалы

--- src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/Person.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/entity/Person.java
@@ -6,6 +6,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @Entity
 @Table(name = "person")
@@ -15,7 +16,7 @@ import java.util.List;
 @AllArgsConstructor
 @ToString
 @Builder
-public class Person {
+public class Person implements SimpleItem{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
@@ -47,21 +48,13 @@ public class Person {
     )
     @ToString.Exclude
     private List<Task> tasks = new ArrayList<>();
-//    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST,
-//            CascadeType.DETACH, CascadeType.REFRESH})
-//    @JoinTable(name = "document_person",
-//            joinColumns = @JoinColumn(name = "person_id"),
-//            inverseJoinColumns = @JoinColumn(name = "document_id"))
-//    @ToString.Exclude
-//    private List<Document> documents = new ArrayList<>();
-
-//    public void addTask(Task task) {
-//        tasks.add(task);
-//    }
-//
-//    public void addDocument(Document document) {
-//        documents.add(document);
-//    }
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(name = "document_person",
+            joinColumns = @JoinColumn(name = "person_id"),
+            inverseJoinColumns = @JoinColumn(name = "document_id"))
+    @ToString.Exclude
+    private List<Document> documents = new ArrayList<>();
 
 
     public Person(String firstName, String lastName, String patronymic, LocalDate birthDay, String department, String position, String rank) {
@@ -72,6 +65,18 @@ public class Person {
         this.department = department;
         this.position = position;
         this.rank = rank;
+    }
+
+    public static String getFIO(Person person){
+        StringBuilder fio = new StringBuilder();
+        fio.append(person.getLastName()).append(" ");
+        if (person.getFirstName() != null && person.getFirstName().length() >= 1){
+            fio.append(person.getFirstName().toUpperCase(Locale.ROOT).charAt(0)).append(".");
+        }
+        if (person.getPatronymic() != null && person.getPatronymic().length() >= 1){
+            fio.append(person.getPatronymic().toUpperCase(Locale.ROOT).charAt(0)).append(".");
+        }
+        return fio.toString();
     }
 
 }
