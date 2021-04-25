2064100f9b4c2d1a69158336776e5305e1e9cb98
kavfox <kavcoffeefox>
2021-04-25 21:26:26 +0300

    Из RootViewController убраны методы для работы с кнопками которые убраны из view

--- src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/RootViewController.java
+++ src/main/java/ru/kavcoffeefox/kcftaskmanager/controllers/RootViewController.java
@@ -1,19 +1,7 @@
 package ru.kavcoffeefox.kcftaskmanager.controllers;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.AnchorPane;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-import ru.kavcoffeefox.kcftaskmanager.entity.Task;
-import ru.kavcoffeefox.kcftaskmanager.service.TaskManager;
-import ru.kavcoffeefox.kcftaskmanager.service.impl.TaskManagerHibernateImpl;
-
-import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -23,85 +11,10 @@ public class RootViewController extends AbstractController {
     @FXML
     private Label nowDataAndTime;
 
-    private TaskManager taskManager;
-
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        taskManager = TaskManagerHibernateImpl.getInstance();
         nowDataAndTime.setText(LocalDate.now().toString());
-    }
-
-    public void actionButtonPressed(ActionEvent actionEvent) {
-        Object source = actionEvent.getSource();
-
-        if (!(source instanceof Button)) {
-            return;
-        }
-
-        Button clickedButton = (Button) source;
-
-        switch (clickedButton.getId()) {
-            case "addtask" -> {
-                Task task = new Task();
-                try {
-                    FXMLLoader loader = new FXMLLoader();
-                    loader.setLocation(RootViewController.class.getResource("/view/modalwindows/TaskView.fxml"));
-                    AnchorPane page = loader.load();
-
-                    Stage dialogStage = new Stage();
-                    dialogStage.setTitle("Добавление");
-                    dialogStage.initModality(Modality.WINDOW_MODAL);
-                    dialogStage.initOwner(this.getMainStage());
-                    Scene scene = new Scene(page);
-                    dialogStage.setScene(scene);
-
-                    TaskViewController controller = loader.getController();
-                    controller.setDialogStage(dialogStage);
-                    controller.setTask(task);
-
-                    dialogStage.showAndWait();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                taskManager.add(task);
-            }
-            case "edittask" -> {
-                try {
-                    FXMLLoader loader = new FXMLLoader();
-                    loader.setLocation(RootViewController.class.getResource("/view/modalwindows/TaskView.fxml"));
-                    AnchorPane page = loader.load();
-
-                    Stage dialogStage = new Stage();
-                    dialogStage.setTitle("Редактирование");
-                    dialogStage.initModality(Modality.WINDOW_MODAL);
-                    dialogStage.initOwner(this.getMainStage());
-                    Scene scene = new Scene(page);
-                    dialogStage.setScene(scene);
-
-                    TaskViewController controller = loader.getController();
-                    controller.setDialogStage(dialogStage);
-                    if(taskManager.getCurrentTask() != null)
-                        controller.setTask(taskManager.getCurrentTask());
-
-                    dialogStage.showAndWait();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                taskManager.update(taskManager.getCurrentTask().getId(), taskManager.getCurrentTask());
-            }
-            case "completetask" -> {
-                if (taskManager.getCurrentTask() != null) {
-                    taskManager.getCurrentTask().setComplete(true);
-                    taskManager.update(taskManager.getCurrentTask().getId(), taskManager.getCurrentTask());
-                }
-            }
-            case "deletetask" -> {
-                if (taskManager.getCurrentTask() != null)
-                    taskManager.delete(taskManager.getCurrentTask().getId());
-            }
-        }
-
     }
 
 
